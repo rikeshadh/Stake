1b3cd539517bebfc9efc6f5cf7778035df66d9f1
rikeshadh <rikeshadhik@gmail.com>
2026-08-27 08:43:25 +0545

    Fix build dependencies

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,4 +1,10 @@
 import serverless from "serverless-http";
 import app from "../../server/app";
 
-export const handler = serverless(app);
+const handlerFn = serverless(app);
+
+export const handler = async (event: any, context: any) => {
+  // Rewrite the Netlify function path back to what Express expects
+  event.path = event.path.replace(/^\/\.netlify\/functions\/api/, "/api") || "/api";
+  return handlerFn(event, context);
+};
